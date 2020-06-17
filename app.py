8a795c5ff4b531cc846f91e5a1c3d6df24be0e31
Pranav <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2020-06-17 10:52:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    #completed = db.Column(db.Integer,default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        taskcontent = request.form['content']
+        new_task = List(content=taskcontent)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect('/')
+    else:
+        tasks = List.query.order_by(List.date_created).all()
+        return render_template('index.html',tasks = tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_todelete = List.query.get_or_404(id)
+    db.session.delete(task_todelete)
+    db.session.commit()
+    return redirect('/')
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
